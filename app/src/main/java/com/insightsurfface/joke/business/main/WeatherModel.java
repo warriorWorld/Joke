f3e8bfbdec7a28fc1f435d4c563ef8ed917ec7c7
warriorWorld <772192594@qq.com>
2020-05-28 23:05:26 +0800

    优雅解决rxjava问题

--- app/src/main/java/com/insightsurfface/joke/business/main/WeatherModel.java
+++ app/src/main/java/com/insightsurfface/joke/business/main/WeatherModel.java
@@ -2,6 +2,7 @@ package com.insightsurfface.joke.business.main;
 
 import android.content.Context;
 
+import com.insightsurfface.joke.base.BaseBean;
 import com.insightsurfface.joke.bean.CityBean;
 import com.insightsurfface.joke.bean.WeatherBean;
 import com.insightsurfface.joke.bean.WeatherTypeBean;
@@ -18,6 +19,7 @@ import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.BiFunction;
 import io.reactivex.functions.Function;
 import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
@@ -41,56 +43,85 @@ public class WeatherModel implements IWeatherModel {
 
     @Override
     public void getWeather(String city, DisposableObserver<WeatherBean> observer) {
-        RetrofitUtil.getInstance(mContext)
-                .create(HttpService.class)
-                .getWeatherTypes(Configures.WEATHER_KEY)
-                .subscribeOn(Schedulers.io())
-                .observeOn(Schedulers.io())
-                .flatMap(new Function<WeatherTypeBean, ObservableSource<WeatherBean>>() {
+        Observable.zip(RetrofitUtil
+                        .getInstance(mContext)
+                        .create(HttpService.class
+                        ).getWeatherTypes(Configures.WEATHER_KEY)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(Schedulers.io()),
+                RetrofitUtil.getInstance(mContext)
+                        .create(HttpService.class)
+                        .getWeather(city, Configures.WEATHER_KEY)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(Schedulers.io())
+                , new BiFunction<WeatherTypeBean, WeatherBean, WeatherBean>() {
                     @Override
-                    public ObservableSource<WeatherBean> apply(WeatherTypeBean weatherTypeBean) throws Exception {
-                        return Observable.create(new ObservableOnSubscribe<WeatherBean>() {
-                            @Override
-                            public void subscribe(ObservableEmitter<WeatherBean> emitter) throws Exception {
-                                RetrofitUtil.getInstance(mContext)
-                                        .create(HttpService.class)
-                                        .getWeather(city, Configures.WEATHER_KEY)
-                                        .subscribeOn(Schedulers.io())
-                                        .observeOn(Schedulers.io())
-                                        .subscribe(new Observer<WeatherBean>() {
-                                            @Override
-                                            public void onSubscribe(Disposable d) {
-
-                                            }
-
-                                            @Override
-                                            public void onNext(WeatherBean bean) {
-                                                for (int i = 0; i < bean.getResult().getFuture().size(); i++) {
-                                                    WeatherBean.ResultBean.FutureBean.WidBean item = bean.getResult().getFuture().get(i).getWid();
-                                                    WeatherBean.ResultBean.FutureBean.WeatherExplainBean explainBean = new WeatherBean.ResultBean.FutureBean.WeatherExplainBean();
-                                                    explainBean.setDay(getWeatherByWid(item.getDay(), weatherTypeBean));
-                                                    explainBean.setNight(getWeatherByWid(item.getNight(), weatherTypeBean));
-                                                    bean.getResult().getFuture().get(i).setwExplain(explainBean);
-                                                }
-                                                emitter.onNext(bean);
-                                            }
-
-                                            @Override
-                                            public void onError(Throwable e) {
-
-                                            }
-
-                                            @Override
-                                            public void onComplete() {
-
-                                            }
-                                        });
-                            }
-                        });
+                    public WeatherBean apply(WeatherTypeBean weatherTypeBean, WeatherBean bean) throws Exception {
+                        for (int i = 0; i < bean.getResult().getFuture().size(); i++) {
+                            WeatherBean.ResultBean.FutureBean.WidBean item = bean.getResult().getFuture().get(i).getWid();
+                            WeatherBean.ResultBean.FutureBean.WeatherExplainBean explainBean = new WeatherBean.ResultBean.FutureBean.WeatherExplainBean();
+                            explainBean.setDay(getWeatherByWid(item.getDay(), weatherTypeBean));
+                            explainBean.setNight(getWeatherByWid(item.getNight(), weatherTypeBean));
+                            bean.getResult().getFuture().get(i).setwExplain(explainBean);
+                        }
+                        return bean;
                     }
                 })
+                .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(observer);
+
+
+//        RetrofitUtil.getInstance(mContext)
+//                .create(HttpService.class)
+//                .getWeatherTypes(Configures.WEATHER_KEY)
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(Schedulers.io())
+//                .flatMap(new Function<WeatherTypeBean, ObservableSource<WeatherBean>>() {
+//                    @Override
+//                    public ObservableSource<WeatherBean> apply(WeatherTypeBean weatherTypeBean) throws Exception {
+//                        return Observable.create(new ObservableOnSubscribe<WeatherBean>() {
+//                            @Override
+//                            public void subscribe(ObservableEmitter<WeatherBean> emitter) throws Exception {
+//                                RetrofitUtil.getInstance(mContext)
+//                                        .create(HttpService.class)
+//                                        .getWeather(city, Configures.WEATHER_KEY)
+//                                        .subscribeOn(Schedulers.io())
+//                                        .observeOn(Schedulers.io())
+//                                        .subscribe(new Observer<WeatherBean>() {
+//                                            @Override
+//                                            public void onSubscribe(Disposable d) {
+//
+//                                            }
+//
+//                                            @Override
+//                                            public void onNext(WeatherBean bean) {
+//                                                for (int i = 0; i < bean.getResult().getFuture().size(); i++) {
+//                                                    WeatherBean.ResultBean.FutureBean.WidBean item = bean.getResult().getFuture().get(i).getWid();
+//                                                    WeatherBean.ResultBean.FutureBean.WeatherExplainBean explainBean = new WeatherBean.ResultBean.FutureBean.WeatherExplainBean();
+//                                                    explainBean.setDay(getWeatherByWid(item.getDay(), weatherTypeBean));
+//                                                    explainBean.setNight(getWeatherByWid(item.getNight(), weatherTypeBean));
+//                                                    bean.getResult().getFuture().get(i).setwExplain(explainBean);
+//                                                }
+//                                                emitter.onNext(bean);
+//                                            }
+//
+//                                            @Override
+//                                            public void onError(Throwable e) {
+//
+//                                            }
+//
+//                                            @Override
+//                                            public void onComplete() {
+//
+//                                            }
+//                                        });
+//                            }
+//                        });
+//                    }
+//                })
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(observer);
     }
 
     private String getWeatherByWid(String id, WeatherTypeBean bean) {
